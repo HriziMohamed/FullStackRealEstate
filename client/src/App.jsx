a3aec600d74ce245f6cd1f804e4235f79ce7aff0
HriziMohamed <mohamed.hrizi@astree.com.tn>
2025-01-31 13:40:35 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { Suspense } from "react";
+import "./App.css";
+import Layout from "./components/Layout/Layout";
+import Website from "./pages/Website";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading....</div>} >
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Website />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+}
+
+export default App;
