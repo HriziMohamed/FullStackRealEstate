ceccf7917fed0f449b5772f8d675bbd786f0a150
HriziMohamed <mohamed.hrizi@astree.com.tn>
2025-01-31 17:30:13 +0100

    commit :

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,19 +4,25 @@ import Layout from "./components/Layout/Layout";
 import Website from "./pages/Website";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Properties from "./pages/properties/Properties";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
+
+  const queryClient = new QueryClient()
+
   return (
-    <BrowserRouter>
-      <Suspense fallback={<div>Loading....</div>} >
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Website />} />
-            <Route path="/properties" element={<Properties />} />
-          </Route>
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading....</div>} >
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Website />} />
+              <Route path="/properties" element={<Properties />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
