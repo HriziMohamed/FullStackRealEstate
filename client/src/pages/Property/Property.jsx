42977bfc0947c9ff376dc73d79d44b8640b17de2
HriziMohamed <mohamed.hrizi@astree.com.tn>
2025-02-06 16:51:54 +0100

    commit : Add property

---
+++ client/src/pages/Property/Property.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Property = () => {
+    return (
+        <div className="wrapper">
+            This is an individual property path
+        </div>
+    )
+}
+
+export default Property;
